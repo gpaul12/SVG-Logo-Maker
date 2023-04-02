70dd053dac2360ac1307603fe2d78a9b78ffa441
Gerry Paul <gpaul12@nc.rr.com>
2023-04-02 15:27:01 -0400

    setTextElement

--- index.js
+++ index.js
@@ -10,4 +10,7 @@ class Svg {
   render() {
     return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`;
   }
+  setTextElement(text, color) {
+    this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`;
+  }
 }
