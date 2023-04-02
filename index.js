aa313a08efb1f5fed42a4e53d3a302b02137196b
Gerry Paul <gpaul12@nc.rr.com>
2023-04-02 15:27:49 -0400

    setShapeElement

--- index.js
+++ index.js
@@ -13,4 +13,7 @@ class Svg {
   setTextElement(text, color) {
     this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`;
   }
+  setShapeElement(shape) {
+    this.shapeElement = shape.render();
+  }
 }
