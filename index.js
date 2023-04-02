e7d93143966b6141e9a57ae5ab5311fe16753b5f
Gerry Paul <gpaul12@nc.rr.com>
2023-04-02 15:25:46 -0400

    render svg

--- index.js
+++ index.js
@@ -7,4 +7,7 @@ class Svg {
     this.textElement = "";
     this.shapeElement = "";
   }
+  render() {
+    return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`;
+  }
 }
