0ff370aa7e0260680ccdbfd7fc8631fb821a101e
Gerry Paul <gpaul12@nc.rr.com>
2023-04-02 15:24:34 -0400

    class svg

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const filesystem = require("./node_modules/graceful-fs/graceful-fs");
 const inquirer = require("inquirer");
 const { Circle, Square, Triangle } = require("./lib/shapes");
+
+class Svg {
+  constructor() {
+    this.textElement = "";
+    this.shapeElement = "";
+  }
+}
